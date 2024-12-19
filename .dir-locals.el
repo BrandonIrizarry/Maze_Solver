075b2ff0708bbc48fca6daa0ade9c580e6acdf12
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-12-19 12:42:16 -0500

    Add .dir-locals to project

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((python-base-mode . ((eglot-workspace-configuration . (:pylsp (:plugins
+
+                                                                (:pyflakes
+                                                                 (:enabled :json-false)
+
+                                                                 :pycodestyle
+                                                                 (:enabled :json-false))))))))
