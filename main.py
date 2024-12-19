77ce3915f57a84f6678d235284636f9be7c9b1c1
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-12-19 15:01:11 -0500

    Implement simple animation using the task-queue

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ class Loop:
     window.
 
     """
-    def __init__(self):
+    def __init__(self, delay_secs=1):
         self.is_running = True
+        self.delay_secs = delay_secs
 
     def run(self, root: tk.Tk, task_queue: list[Task]):
         def close_handler():
@@ -34,15 +35,40 @@ class Loop:
                 task = task_queue.pop(0)
                 task()
 
-            time.sleep(2)
+            time.sleep(self.delay_secs)
+
+
+class Line:
+    def __init__(self, canvas: tk.Canvas, x1, y1, x2, y2):
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+        self.canvas = canvas
+
+    def create(self):
+        self.line = self.canvas.create_line(self.x1,
+                                            self.y1,
+                                            self.x2,
+                                            self.y2,
+                                            width=2,
+                                            fill="red")
+
+    def hide(self):
+        self.canvas.itemconfig(self.line, fill="gray")
 
 
 if __name__ == "__main__":
-    loop = Loop()
+    loop = Loop(delay_secs=0.5)
+
+    line1 = Line(canvas, 20, 30, 100, 150)
+    line2 = Line(canvas, 70, 80, 120, 140)
 
     task_queue = [
-        lambda: canvas.create_line(20, 30, 40, 50, width=2, fill="red"),
-        lambda: canvas.create_line(70, 80, 90, 100, width=3, fill="blue")
+        lambda: line1.create(),
+        lambda: line2.create(),
+        lambda: line1.hide(),
+        lambda: line2.hide()
     ]
 
     loop.run(root, task_queue)
